f8352cff54f242811124e72216e32fd046d55508
Grampers-Dev <introspection31@gmail.com>
2024-05-06 23:01:14 +0100

    Update SECRET_KEY configuration in settings.py

--- tangle/tangle/settings.py
+++ tangle/tangle/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l3z1!n9qln78x&9)11ouqfrl#$mil@=r%+j7u^feo%3^rt!8tv'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
