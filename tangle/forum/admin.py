4b518822a8049b6667ddd2dc085e4d731430cc15
Grampers-Dev <introspection31@gmail.com>
2024-05-11 20:46:28 +0100

    Update admin.py to register Profile model in user_profile app

--- tangle/forum/admin.py
+++ tangle/forum/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
+from .models import Thread, Reply, Profile
 
-from .models import Thread, Reply
+class ThreadAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_created', 'total_likes', 'total_dislikes')
+    list_filter = ('date_created',)
+    search_fields = ('title', 'description')
 
-admin.site.register(Thread)
+class ReplyAdmin(admin.ModelAdmin):
+    list_display = ('thread', 'user', 'date_created', 'message')
+    list_filter = ('date_created', 'user')
+    search_fields = ('message', 'user__username', 'thread__title')
 
-admin.site.register(Reply)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+    search_fields = ('user__username', 'user__email')
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Thread, ThreadAdmin)
+admin.site.register(Reply, ReplyAdmin)
