02d6815ef9c0793e55938a1b364945b651882c2c
Grampers-Dev <introspection31@gmail.com>
2024-05-20 17:51:20 +0100

    Add ProfileForm for user profile updates

--- tangle/user_profile/forms.py
+++ tangle/user_profile/forms.py
@@ -1,7 +1,9 @@
 from django import forms
+from django.forms import ModelForm 
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-
+from forum.models import Profile
+from user_profile.models import Profile 
 
 class ExtendedUserCreationForm(UserCreationForm):
     """
@@ -110,3 +112,9 @@ class ExtendedAuthenticationForm(AuthenticationForm):
             'placeholder': 'Enter a Password',
             'tabindex': '2'
         })
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio', 'location', 'birth_date', 'image']
