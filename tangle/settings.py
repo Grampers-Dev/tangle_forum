57416a72a289c5734ce11bf167534ae6994c1fb0
Grampers-Dev <introspection31@gmail.com>
2024-05-25 15:48:28 +0100

    Remove X-Frame-Options header in settings.py, temporay fix for responsive design

--- tangle/settings.py
+++ tangle/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-X_FRAME_OPTIONS = "SAMEORIGIN"
+X_FRAME_OPTIONS = ""
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
